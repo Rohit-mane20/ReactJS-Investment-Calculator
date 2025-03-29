4ac4778e2b3b429a74922f2c807460187b55e205
Rohit Mane <rohitmane406@gmail.com>
2025-03-29 13:37:33 +0530

    added strict mode

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,7 +1,13 @@
 import { Fragment } from "react";
 import { calculateInvestmentResults, formatter } from "./util/investment";
+// const resultsData = []
 export default function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
+
+  if(resultsData.length === 0){
+    return <p className="center">Invalid input data provide</p>
+  }
+
   const initialInvestment =
     resultsData[0].valueEndOfYear -
     resultsData[0].interest -
